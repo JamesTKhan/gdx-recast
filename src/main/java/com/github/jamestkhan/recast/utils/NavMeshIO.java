c614c9902c7438a632673ecf7176488ee4832018
Anthony Iantria <anthony.iantria@gmail.com>
2023-09-01 16:16:52 -0400

    Use com.badlogic.gdx.files.FileHandle on load/save IO

--- src/main/java/com/github/jamestkhan/recast/utils/NavMeshIO.java
+++ src/main/java/com/github/jamestkhan/recast/utils/NavMeshIO.java
@@ -1,13 +1,12 @@
 package com.github.jamestkhan.recast.utils;
 
+import com.badlogic.gdx.files.FileHandle;
+
 import org.recast4j.detour.NavMesh;
 import org.recast4j.detour.io.MeshSetReader;
 import org.recast4j.detour.io.MeshSetWriter;
-
-import java.io.File;
 import java.io.IOException;
 import java.nio.ByteOrder;
-import java.nio.file.Files;
 
 /**
  * Simple utility class for saving and loading NavMeshes.
@@ -16,13 +15,25 @@ import java.nio.file.Files;
  */
 public class NavMeshIO {
 
-    public static void save(NavMesh navMesh, File file) throws IOException {
-        MeshSetWriter writer = new MeshSetWriter();
-        writer.write(Files.newOutputStream(file.toPath()), navMesh, ByteOrder.BIG_ENDIAN, false);
+//    public static void save(NavMesh navMesh, File file) throws IOException {
+//        MeshSetWriter writer = new MeshSetWriter();
+//        writer.write(Files.newOutputStream(file.toPath()), navMesh, ByteOrder.BIG_ENDIAN, false);
+//    }
+//
+//    public static NavMesh load(File file) throws IOException {
+//        MeshSetReader reader = new MeshSetReader();
+//        return reader.read(Files.newInputStream(file.toPath()), 3);
+//    }
+
+    // Use com.badlogic.gdx.files.FileHandle
+
+    public static NavMesh load(FileHandle file) throws IOException {
+        MeshSetReader reader = new MeshSetReader();
+        return reader.read(file.read(), 3);
     }
 
-    public static NavMesh load(File file) throws IOException {
-        MeshSetReader reader = new MeshSetReader();
-        return reader.read(Files.newInputStream(file.toPath()), 3);
+    public static void save(NavMesh navMesh, FileHandle file) throws IOException {
+        MeshSetWriter writer = new MeshSetWriter();
+        writer.write(file.write(false), navMesh, ByteOrder.BIG_ENDIAN, false);
     }
 }
