b8c12e3fb8d44e6d601351b5be25c71277522ef0
James <wjamesfl@gmail.com>
2023-09-15 13:30:15 -0400

    ModelBatch DebugDraw

--- src/main/java/com/github/jamestkhan/recast/debug/DebugDraw.java
+++ src/main/java/com/github/jamestkhan/recast/debug/DebugDraw.java
@@ -4,6 +4,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.BlendingAttribute;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
@@ -21,23 +30,161 @@ import org.recast4j.detour.crowd.CrowdAgent;
  */
 public class DebugDraw {
 
-    public final CustomShapeRenderer shapeRenderer = new CustomShapeRenderer();
+    public final CustomShapeRenderer shapeRenderer;
     private final Array<Float> vertices = new Array<>();
+    private ModelBatch modelBatch = new ModelBatch();
+    private Model navMeshModel;
+    private ModelInstance navMeshInstance;
+    private int polyFlags;
+    private NavMesh currentNavMesh;
+    private Camera cam;
 
-    public void begin(Camera camera) {
+    private final Color baseTileColor = Color.TEAL;
+    private final Color[] tileColors = new Color[]{
+            baseTileColor,
+            baseTileColor.cpy().lerp(Color.BLUE, 0.2f),
+            baseTileColor.cpy().lerp(Color.BLACK, 0.2f),
+            baseTileColor.cpy().lerp(Color.BLUE, 0.4f),
+            baseTileColor.cpy().lerp(Color.BLACK, 0.4f),
+            baseTileColor.cpy().lerp(Color.BLUE, 0.6f),
+            baseTileColor.cpy().lerp(Color.BLACK, 0.6f),
+    };
+
+    private Material lineMaterial;
+    private Material meshMaterial;
+
+    public DebugDraw(Camera camera) {
+        cam = camera;
+        shapeRenderer = new CustomShapeRenderer();
+        shapeRenderer.setAutoShapeType(true);
+    }
+
+    public void debugDrawNavMeshPolysWithFlags(NavMesh mesh, int polyFlags) {
+        // Do we need to rebuild the navmesh model?
+        if (currentNavMesh != mesh || this.polyFlags != polyFlags) {
+            currentNavMesh = mesh;
+            this.polyFlags = polyFlags;
+
+            if (navMeshModel != null) navMeshModel.dispose();
+
+            navMeshModel = buildNavMeshModel(mesh, polyFlags);
+            navMeshInstance = new ModelInstance(navMeshModel);
+        }
+
+        modelBatch.begin(cam);
+        modelBatch.render(navMeshInstance);
+        modelBatch.end();
+    }
+
+    public void drawCrowdAgents(Array<CrowdAgent> crowdAgents) {
+        setOpenGLState();
+        shapeRenderer.begin();
+        shapeRenderer.setProjectionMatrix(cam.combined);
+        shapeRenderer.setColor(Color.FOREST);
+        shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
+
+        for (CrowdAgent agent : crowdAgents) {
+            float radius = agent.params.radius;
+            float height = agent.params.height;
+            shapeRenderer.box(agent.npos[0], agent.npos[1], agent.npos[2], radius, height, radius);
+        }
+
+        shapeRenderer.end();
+    }
+
+    private void setOpenGLState() {
         Gdx.gl.glDepthFunc(GL20.GL_LESS);
         Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
         Gdx.gl.glEnable(GL20.GL_BLEND);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-        shapeRenderer.setProjectionMatrix(camera.combined);
-        shapeRenderer.setAutoShapeType(true);
-        shapeRenderer.begin();
     }
 
-    public void debugDrawNavMeshPolysWithFlags(NavMesh mesh, int polyFlags) {
-        vertices.clear();
-        shapeRenderer.setColor(Color.TEAL.r, Color.TEAL.g, Color.TEAL.b, .4f);
+    public void renderStartPos(Vector3 start) {
+        renderStartPos(start.x, start.y, start.z);
+    }
 
+    public void renderStartPos(float x, float y, float z) {
+        setOpenGLState();
+        shapeRenderer.begin();
+        shapeRenderer.setProjectionMatrix(cam.combined);
+        shapeRenderer.setColor(Color.GREEN);
+        shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
+        renderBoxCentered(x, y, z, 2f, 2f, 2f);
+        shapeRenderer.end();
+    }
+
+    public void renderEndPos(Vector3 start) {
+        renderEndPos(start.x, start.y, start.z);
+    }
+
+    public void renderEndPos(float x, float y, float z) {
+        setOpenGLState();
+        shapeRenderer.begin();
+        shapeRenderer.setProjectionMatrix(cam.combined);
+        shapeRenderer.setColor(Color.RED);
+        shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
+        renderBoxCentered(x, y, z, 2f, 2f, 2f);
+        shapeRenderer.end();
+    }
+
+    public void renderPath(Array<float[]> paths) {
+        renderPath(paths, true);
+    }
+
+    public void renderPath(Array<float[]> paths, boolean renderStartAndEnd) {
+        setOpenGLState();
+        shapeRenderer.begin();
+        shapeRenderer.setProjectionMatrix(cam.combined);
+        for (int i = 1; i < paths.size; i += 2) {
+
+            if (!renderStartAndEnd) {
+                if (i == 1) {
+                    continue;
+                } else if (i >= paths.size - 2) {
+                    continue;
+                }
+            }
+            shapeRenderer.setColor(Color.BLUE);
+
+            float[] pos = paths.get(i - 1);
+            float[] nextPos = paths.get(i);
+
+            renderLine(pos[0], pos[1], pos[2], nextPos[0], nextPos[1], nextPos[2], 0f);
+        }
+        shapeRenderer.end();
+    }
+
+    private void renderLine(float x1, float y1, float z1, float x2, float y2, float z2, float offset) {
+        shapeRenderer.line(x1 + offset, y1 + 0.5f, z1 + offset, x2 + offset, y2 + 0.5f, z2 + offset);
+    }
+
+    private void renderBoxCentered(float x, float y, float z, float width, float height, float depth) {
+        float halfWidth = width / 2.0f;
+        float halfDepth = depth / 2.0f;
+        shapeRenderer.box(x - halfWidth, y, z + halfDepth, width, height, depth);
+    }
+
+    private Model buildNavMeshModel(NavMesh mesh, int polyFlags) {
+        if (lineMaterial == null) {
+            lineMaterial = new Material();
+            lineMaterial.set(ColorAttribute.createDiffuse(Color.BLACK));
+            lineMaterial.set(new BlendingAttribute(true, .1f));
+        }
+
+        if (meshMaterial == null) {
+            meshMaterial = new Material();
+            meshMaterial.set(new BlendingAttribute(true, .3f));
+        }
+
+        // The height offset is used to raise the navmesh off the surface so it's not clipping into the surface
+        float heightOffset = 0.1f;
+
+        ModelBuilder modelBuilder = new ModelBuilder();
+        modelBuilder.begin();
+
+        Array<Integer> tileIndexes = new Array<>();
+
+        // accumulate vertices
         for (int i = 0; i < mesh.getMaxTiles(); ++i) {
             MeshTile tile = mesh.getTile(i);
             if (tile == null || tile.data == null || tile.data.header == null) {
@@ -45,26 +192,89 @@ public class DebugDraw {
             }
             long base = mesh.getPolyRefBase(tile);
 
+            // Filter by flags
             for (int j = 0; j < tile.data.header.polyCount; ++j) {
                 Poly p = tile.data.polys[j];
                 if ((p.flags & polyFlags) == 0) {
                     continue;
                 }
-                debugDrawNavMeshPoly(mesh, base | j);
+                addVertices(mesh, base | j);
             }
+
+            tileIndexes.add(vertices.size);
         }
 
-        int i = 0;
-        float heightOffset = 0.2f;
-        while (i < vertices.size) {
-            shapeRenderer.line(vertices.get(i), vertices.get(i+1) + heightOffset, vertices.get(i+2), vertices.get(i+3), vertices.get(i+4) + heightOffset, vertices.get(i+5));
-            shapeRenderer.line(vertices.get(i+3), vertices.get(i+4) + heightOffset, vertices.get(i+5), vertices.get(i+6), vertices.get(i+7) + heightOffset, vertices.get(i+8));
-            shapeRenderer.line(vertices.get(i+6), vertices.get(i+7) + heightOffset, vertices.get(i+8), vertices.get(i), vertices.get(i+1) + heightOffset, vertices.get(i+2));
-            i+= 9;
+        Array<Vector3> tris = new Array<>();
+        MeshPartBuilder mpb = modelBuilder.part("navMesh", GL20.GL_TRIANGLES, VertexAttributes.Usage.Position | VertexAttributes.Usage.ColorUnpacked, meshMaterial);
+
+        // Set first tile color
+        mpb.setColor(tileColors[0]);
+
+        int j = 0;
+        while (j < vertices.size) {
+
+            if (j > 0 && j % 32400 == 0) {
+                // Every 32,400 vertices, create line mesh and make a new navmesh part
+                createLineMesh(modelBuilder, tris);
+                tris.clear();
+
+                mpb = modelBuilder.part("navMesh", GL20.GL_TRIANGLES, VertexAttributes.Usage.Position | VertexAttributes.Usage.ColorUnpacked, meshMaterial);
+            }
+
+            float x1 = vertices.get(j);
+            float y1 = vertices.get(j + 1) + heightOffset;
+            float z1 = vertices.get(j + 2);
+
+            float x2 = vertices.get(j + 3);
+            float y2 = vertices.get(j + 4) + heightOffset;
+            float z2 = vertices.get(j + 5);
+
+            float x3 = vertices.get(j + 6);
+            float y3 = vertices.get(j + 7) + heightOffset;
+            float z3 = vertices.get(j + 8);
+
+            Vector3 v1 = new Vector3(x1, y1, z1);
+            Vector3 v2 = new Vector3(x2, y2, z2);
+            Vector3 v3 = new Vector3(x3, y3, z3);
+
+            int tileIdx = tileIndexes.get(0);
+            if (j >= tileIdx) {
+                mpb.setColor(tileColors[tileIdx % tileColors.length]);
+                tileIndexes.removeIndex(0);
+            }
+
+            mpb.triangle(v1, v2, v3);
+
+            // Track tris for creating line mesh
+            tris.add(v1);
+            tris.add(v2);
+            tris.add(v3);
+            j += 9;
+        }
+
+        createLineMesh(modelBuilder, tris);
+
+        vertices.clear();
+        return modelBuilder.end();
+    }
+
+    private void createLineMesh(ModelBuilder modelBuilder, Array<Vector3> tris) {
+        MeshPartBuilder lineBuilder = modelBuilder.part("navMeshLines", GL20.GL_LINES, VertexAttributes.Usage.Position | VertexAttributes.Usage.ColorUnpacked, lineMaterial);
+        for (int k = 0; k < tris.size; k += 3) {
+            Vector3 v1 = tris.get(k);
+            Vector3 v2 = tris.get(k + 1);
+            Vector3 v3 = tris.get(k + 2);
+
+            lineBuilder.line(v1, v2);
+            lineBuilder.line(v2, v3);
+            lineBuilder.line(v3, v1);
         }
     }
 
-    public void debugDrawNavMeshPoly(NavMesh mesh, long ref) {
+    /**
+     * Add vertices for a given NavMesh to the vertices array.
+     */
+    public void addVertices(NavMesh mesh, long ref) {
         if (ref == 0) {
             return;
         }
@@ -76,14 +286,9 @@ public class DebugDraw {
         MeshTile tile = tileAndPoly.first;
         Poly poly = tileAndPoly.second;
 
-        Gdx.gl.glDepthMask((false));
-
-       // int c = duTransCol(col, 64);
-        int c = 0;
         int ip = poly.index;
 
         if (poly.getType() == Poly.DT_POLYTYPE_OFFMESH_CONNECTION) {
-            System.out.println("off");
 //            OffMeshConnection con = tile.data.offMeshCons[ip - tile.data.header.offMeshBase];
 //
 //            begin(DebugDrawPrimitives.LINES, 2.0f);
@@ -94,13 +299,15 @@ public class DebugDraw {
 //
 //            end();
         } else {
-            drawPoly(tile, ip);
+            addVertices(tile, ip);
         }
 
-        Gdx.gl.glDepthMask((true));
     }
 
-    private void drawPoly(MeshTile tile, int index) {
+    /**
+     * Add vertices for a given tile to the vertices array.
+     */
+    private void addVertices(MeshTile tile, int index) {
         Poly p = tile.data.polys[index];
         if (tile.data.detailMeshes != null) {
             PolyDetail pd = tile.data.detailMeshes[index];
@@ -132,81 +339,9 @@ public class DebugDraw {
         }
     }
 
-    public void drawCrowdAgents(Array<CrowdAgent> crowdAgents) {
-        shapeRenderer.setColor(Color.FOREST);
-        shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
-
-        for (CrowdAgent agent : crowdAgents) {
-            float radius = agent.params.radius;
-            float height = agent.params.height;
-            shapeRenderer.box(agent.npos[0], agent.npos[1], agent.npos[2], radius, height, radius);
-        }
-    }
-
     public void vertex(float x, float y, float z) {
         vertices.add(x);
         vertices.add(y);
         vertices.add(z);
     }
-
-    public void end() {
-        shapeRenderer.end();
-        Gdx.gl.glEnable(GL20.GL_BLEND);
-    }
-
-    public void renderStartPos(Vector3 start) {
-        renderStartPos(start.x, start.y, start.z);
-    }
-
-    public void renderStartPos(float x, float y, float z) {
-        shapeRenderer.setColor(Color.GREEN);
-        shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
-        renderBoxCentered(x, y, z, 2f, 2f, 2f);
-        shapeRenderer.set(ShapeRenderer.ShapeType.Line);
-    }
-
-    public void renderEndPos(Vector3 start) {
-        renderEndPos(start.x, start.y, start.z);
-    }
-
-    public void renderEndPos(float x, float y, float z) {
-        shapeRenderer.setColor(Color.RED);
-        shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
-        renderBoxCentered(x, y, z, 2f, 2f, 2f);
-        shapeRenderer.set(ShapeRenderer.ShapeType.Line);
-    }
-
-    public void renderPath(Array<float[]> paths) {
-        renderPath(paths, true);
-    }
-
-    public void renderPath(Array<float[]> paths, boolean renderStartAndEnd) {
-        for (int i = 1; i < paths.size; i +=2) {
-
-            if (!renderStartAndEnd) {
-                if (i == 1) {
-                    continue;
-                } else if (i >= paths.size - 2) {
-                    continue;
-                }
-            }
-            shapeRenderer.setColor(Color.BLUE);
-
-            float[] pos = paths.get(i - 1);
-            float[] nextPos = paths.get(i);
-
-            renderLine(pos[0], pos[1], pos[2],nextPos[0], nextPos[1], nextPos[2], 0f);
-        }
-    }
-
-    private void renderLine(float x1, float y1, float z1, float x2, float y2, float z2, float offset) {
-        shapeRenderer.line(x1 + offset, y1 + 0.5f, z1 + offset, x2 + offset, y2 + 0.5f, z2 + offset);
-    }
-
-    private void renderBoxCentered(float x, float y, float z, float width, float height, float depth) {
-        float halfWidth = width / 2.0f;
-        float halfDepth = depth / 2.0f;
-        shapeRenderer.box(x - halfWidth, y, z + halfDepth, width, height, depth);
-    }
-
 }
